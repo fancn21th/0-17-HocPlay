8eafd473a296822af8d1590226431b9a80ff7f0b
alex.fan <alex.fan@okchem.com>
2017-05-21 22:54:06 +0800

    1. First HOC

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const hoc = (BaseComponent) => (props) => (
+    <BaseComponent {...props} />
+)
+
+const NormalComponent = ({ name }) => (
+    <div className="box">
+        {name}
+    </div>
+)
+
+const NormalComponent2 = hoc(NormalComponent)
 
 class App extends Component {
   render() {
+    const props = {
+        name: 'max'
+    }
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          <NormalComponent {...props} />
+          <NormalComponent2 name="Joe"/>
       </div>
     );
   }
